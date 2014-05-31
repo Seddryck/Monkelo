ccce8a27a9fd43cb13357ff32985bdba3a1f2ada
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-05-31 16:40:47 +0200

    First version of the fact FactRowCached

---
+++ src/Monkelo.Database/stg/FactRowCached.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [stg].[FactRowCached]
+(
+	[PackageId] INT NOT NULL
+	,[Component] varchar(255) NOT NULL
+	,[RowCount] BIGINT NOT NULL
+	,[DurationMilliSecond] INT NOT NULL
+	,[MemoryAllocated] BIGINT NOT NULL
+)
